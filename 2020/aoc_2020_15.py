f251a88a6a742394adc996f4804eecd3532a1697
LeppyR64 <jlepack@gmail.com>
2020-12-15 03:36:11 -0500

    Advent of Code Day 15

---
+++ 2020/aoc_2020_15.py
@@ -0,0 +1,43 @@
+# Advent of Code 2020 - Day 15
+# https://adventofcode.com/2020/day/15
+
+def process_case(inp, depth):
+    mem = {}
+    mem2 = {}
+    digits = [int(x) for x in inp.split(',')]
+    idx = 1
+    for nxt in digits:
+        mem[nxt] = idx
+        idx += 1
+        last = nxt
+
+    while idx <= depth:
+        if last not in mem2:
+            nxt = 0
+        else:
+            nxt = mem[last] - mem2[last]
+        if nxt in mem:
+            mem2[nxt] = mem[nxt]
+        mem[nxt] = idx
+        idx += 1
+        last = nxt
+    return last
+
+print('part 1', process_case('12,20,0,6,1,17,7', 2020))
+print('part 2', process_case('12,20,0,6,1,17,7', 30000000))
+#print(process_case('0,3,6', 10)) #0
+#print(process_case('0,3,6', 2020)) #436
+#print(process_case('1,3,2', 2020)) #1
+#print(process_case('2,1,3', 2020)) #10
+#print(process_case('1,2,3', 2020)) #27
+#print(process_case('2,3,1', 2020)) #78
+#print(process_case('3,2,1', 2020)) #438
+#print(process_case('3,1,2', 2020)) #1836
+#print(process_case('0,3,6', 30000000)) #175594
+#print(process_case('1,3,2', 30000000)) #2578
+#print(process_case('2,1,3', 30000000)) #3544142
+#print(process_case('1,2,3', 30000000)) #261214
+#print(process_case('2,3,1', 30000000)) #6895259
+#print(process_case('3,2,1', 30000000)) #18
+#print(process_case('3,1,2', 30000000)) #362
+
