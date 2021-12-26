2bb4aa254fce7b491e430d666ebc91b84301c868
LeppyR64 <jlepack@gmail.com>
2021-12-26 00:43:11 -0500

    Correcting part 1 of 2021 Day 22

--- 2021/aoc_2021_21.py
+++ 2021/aoc_2021_21.py
@@ -10,6 +10,42 @@ f.close()
 wins = dict()
 losses = dict()
 
+def rolldice(dice):
+    roll = 3*dice + 3
+    if dice == 98:
+        roll = 3*dice + 3
+        dice = 1
+    elif dice == 99:
+        roll = 99 + 100 + 1
+        dice = 2
+    elif dice == 100:
+        roll = 100 + 1 + 2
+        dice = 3
+    else:
+        roll = 3*dice + 3
+        dice = dice + 3
+    return dice, roll
+
+def part1(posa, posb):
+    posa -= 1
+    posb -= 1
+    scorea = 0
+    scoreb = 0
+    dice = 1
+    totalrolls = 0
+    while scorea < 1000 and scoreb < 1000:
+        dice, roll = rolldice(dice)
+        posa = (posa + roll) % 10
+        scorea += posa + 1
+        totalrolls += 3
+        if scorea < 1000:
+            dice, roll = rolldice(dice)
+            posb = (posb + roll) % 10
+            scoreb += posb + 1
+            totalrolls += 3
+    result = min(scorea, scoreb) * totalrolls
+    return result
+
 def calculate_wins(scorea, scoreb, posa, posb):
     
     if (scorea, scoreb, posa, posb) in wins:
@@ -50,10 +86,14 @@ def calculate_wins(scorea, scoreb, posa, posb):
     losses[(scorea, scoreb, posa, posb)] = nlosses
     return result
 
+
+print('Part 1', part1(4, 5)) 
+
 totwins = calculate_wins(0, 0, 4, 8)
 totlosses = losses[(0, 0, 4, 8)]
-print('Part 1', totwins, totlosses)
+print('Part 2 Sample', totwins, totlosses)
 
 totwins = calculate_wins(0, 0, 4, 5)
 totlosses = losses[(0, 0, 4, 5)]
 print('Part 2', totwins, totlosses)
+
